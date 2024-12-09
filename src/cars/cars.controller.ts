753db7cad7f1c7c45ae49292c5ee42f31016f95f
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-12-09 14:27:38 -0600

    Implementing Post, Patch and Delete methods

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,23 +1,45 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { ok } from 'assert';
 
 @Controller('cars')
 export class CarsController {
 
     constructor(private carsService: CarsService) {}
 
+    
     @Get()
-    getAllCards() {
+    getAllCards() 
+    {
         return this.carsService.findAll();
     }
 
     @Get(':id')
-    getCardById(@Param('id', ParseIntPipe) id: number) {
-        console.log({id});
-        
-        // throw new Error('Error message');
+    getCardById(@Param('id', ParseIntPipe) id: number) 
+    {
         return {
             'car': this.carsService.findById(id)
+        }
+    }
+
+    @Post()
+    createCard(@Body() body: any)
+    {
+        return body
+    }
+
+    @Patch(':id')
+    updateCAr(@Param('id', ParseIntPipe) id:number, @Body() body: any)
+    {
+        return body
+    }
+
+    @Delete(':id')
+    deleteCard(@Param('id', ParseIntPipe) id: number)
+    {
+        return {
+            method: 'delete',
+            id: id
         }
     }
 }
